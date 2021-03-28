044da8670e1b9405ca39571a6686a6a925c9b923
Kofi Poku <kap029@bucknell.edu>
2021-03-28 12:00:51 -0400

    Starting the basic version of burger builder app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'; 
+
 // function App() {
 //   return (
 //     <div className="App">
@@ -11,7 +14,9 @@ class App extends Component {
   render() {
     return(
       <div>
-
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
     )
   }
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import Aux from '../../hoc/Aux';
+
+class BurgerBuilder extends Component {
+    render() {
+        return(
+            <Aux>
+                <div>
+                    Graphics for the Burger Built
+                </div>
+                <div>Build Controls</div>
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
