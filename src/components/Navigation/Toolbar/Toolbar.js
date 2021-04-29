f72d56c40cd0efdf78db118e06431df30010a620
Kofi Poku <kap029@bucknell.edu>
2021-04-29 12:00:03 -0400

    Controlling logo height through CSS classes

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -7,7 +7,10 @@ import classes from './Toolbar.module.css';
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
-        <Logo height="80%" />
+        <div className={classes.Logo}>
+            <Logo height="80%" />
+        </div>
+
         <nav>
             <NavigationItems />
         </nav>
