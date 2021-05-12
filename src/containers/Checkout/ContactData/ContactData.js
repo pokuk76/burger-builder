ff379161c92f7424bfe372039405488055602ad4
Kofi Poku <kap029@bucknell.edu>
2021-05-12 18:02:57 -0400

    Accessing Protected Resources (sending the JWT as a query param to the endpoint)

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -116,7 +116,7 @@ class ContactData extends Component {
             order: formData,
         };
 
-        this.props.onOrderBurger(order);
+        this.props.onOrderBurger(order, this.props.token);
     }
 
     checkValidity(value, rules) {
@@ -212,13 +212,14 @@ const mapStateToProps = state => {
     return {
         ingredients: state.burgerBuilder.ingredients,
         price: state.burgerBuilder.totalPrice,
-        loading: state.order.loading
+        loading: state.order.loading,
+        token: state.auth.token, 
     }
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-        onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData)),
+        onOrderBurger: (orderData, token) => dispatch( actions.purchaseBurger(orderData, token) ),
     };
 };
 
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -10,8 +10,9 @@ import * as actions from '../../store/actions/index';
 class Orders extends Component {
 
     componentDidMount() {
-        this.props.onFetchOrders();
+        this.props.onFetchOrders(this.props.token);
     }
+    
     render() {
         
         let orders = <Spinner />;
@@ -35,12 +36,13 @@ const mapStateToProps = state => {
     return {
         orders: state.order.orders,
         loading: state.order.loading,
+        token: state.auth.token,
     }
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-        onFetchOrders: () => dispatch( actions.fetchOrders() ),
+        onFetchOrders: (token) => dispatch( actions.fetchOrders(token) ),
     };
 };
 
