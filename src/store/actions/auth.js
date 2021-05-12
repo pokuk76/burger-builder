95af3ce8f93e0b314e0ace10f3228f8184a9372e
Kofi Poku <kap029@bucknell.edu>
2021-05-12 15:43:12 -0400

    Logging Users Out

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -23,6 +23,21 @@ export const authFail = (error) => {
     };
 };
 
+export const logout = () => {
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    };
+};
+
+export const checkAuthTimeout = (expirationTime) => {
+    let expirationTimeMillisec = expirationTime * 1000;
+    return dispatch => {
+        setTimeout(() => {
+            dispatch( logout() );
+        }, expirationTimeMillisec);
+    };
+};
+
 export const auth = (email, password, isSignup) => {
     return dispatch => {
         dispatch( authStart() );
@@ -41,6 +56,7 @@ export const auth = (email, password, isSignup) => {
             .then( response => {
                 console.log(response);
                 dispatch( authSuccess(response.data.idToken, response.data.localId) );
+                dispatch( checkAuthTimeout(response.data.expiresIn) );
             })
             .catch( error => {
                 console.log(error);
