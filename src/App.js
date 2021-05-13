c23df5c606154ef410b70823700bde494b9a8388
Kofi Poku <kap029@bucknell.edu>
2021-05-13 06:01:26 -0400

    Persistent auth state with localStorage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'; 
@@ -7,6 +8,8 @@ import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
+
+import * as actions from './store/actions/index';
 
 // function App() {
 //   return (
@@ -16,6 +19,11 @@ import Logout from './containers/Auth/Logout/Logout';
 // }
 
 class App extends Component {
+
+  componentDidMount() {
+    this.props.onTryAutoLogin();
+  }
+  
   render() {
     return(  
       <div>
@@ -35,4 +43,10 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoLogin: () => dispatch( actions.authCheckState() ), 
+  };
+};
+
+export default connect(null, mapDispatchToProps)(App);
