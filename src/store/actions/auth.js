6f0f91303ce97463154f8b529232d59b043960d9
Kofi Poku <kap029@bucknell.edu>
2021-05-13 09:28:42 -0400

    Fixed logout on refresh error

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -85,7 +85,7 @@ export const authCheckState = () => {
         if (!token) {
             dispatch( logout() );
         } else {
-            const expirationDate = new Date(localStorage.getItem('expirationTime'));
+            const expirationDate = new Date(localStorage.getItem('expirationDate'));
             if (expirationDate <= new Date()){
                 dispatch( logout() );
             } else {
