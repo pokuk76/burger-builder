7297fb0c3586691987d9548536f49cf1f8931221
Kofi Poku <kap029@bucknell.edu>
2021-05-25 07:39:49 -0400

    Just removing some console.log()s

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -57,7 +57,7 @@ export const auth = (email, password, isSignup) => {
 
         axios.post(authAPI_URL, authData)
             .then( response => {
-                console.log(response);
+                // console.log(response);
                 const expirationDate = new Date(new Date().getTime() + (response.data.expiresIn * 1000));
                 localStorage.setItem('token', response.data.idToken);
                 localStorage.setItem('expirationDate', expirationDate);
@@ -66,7 +66,7 @@ export const auth = (email, password, isSignup) => {
                 dispatch( checkAuthTimeout(response.data.expiresIn) );
             })
             .catch( error => {
-                console.log(error);
+                // console.log(error);
                 dispatch( authFail(error.response.data.error) );
             });
     };
