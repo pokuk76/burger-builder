4f1ff6d2660c98723c463e86aeabe7a79094ade3
Kofi Poku <kap029@bucknell.edu>
2021-05-25 10:19:08 -0400

    one more test for NavigationItems

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -25,4 +25,9 @@ describe('<NavigationItems />', () => {
         wrapper.setProps({isAuthenticated: true});
         expect(wrapper.find(NavigationItem)).toHaveLength(3);
     });
+
+    it('should render a <NavigationItem/> element with logout functionality if authenticated', () => {
+        wrapper.setProps({isAuthenticated: true});
+        expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
+    });
 });
